3ea39b15e20646b0a920f41d5715481cba9dcc8e
emilwihlander <emil.wihlander@outlook.com>
2016-04-30 04:05:04 +0200

    Revert "Changed getInfo method"
    This reverts commit c490e6c6643899a2eeb377c0a38a05ebe00d31e2.

--- src/docxToTexConverter/docxToTex.scala
+++ src/docxToTexConverter/docxToTex.scala
@@ -8,14 +8,14 @@ object docxToTex {
   def main(args:Array[String]): Unit = {
     if (args.length != 1)
       throw new IllegalArgumentException("There isn't exactly one agrument")
-
+    
     val input = new File(args(0))
     println(input.getAbsolutePath);
     if (!input.exists()) //Checks if the file exists
-      throw new IllegalArgumentException("File doesn't exist")
+      throw new IllegalArgumentException("File doesn't exist") 
     if (!input.getPath.endsWith(".docx")) //Checks if the file is a .docx file
       throw new IllegalArgumentException("File name doesn't end with .docx")
-
+    
     val uz = new Unzipper(input)
     implicit val folder = uz.run //Unzips input and saves the unzip-folder path in folder
     val da = new DocxAnalyser
